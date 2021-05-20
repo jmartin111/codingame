bba8c30e45f1a67a55f0c4d154a2b0f676027c50
Jeff <chess.logic@gmail.com>
2021-05-20 16:29:32 -0700

    Updates to how IP binaries are calculated and masked

--- ip_address_masks.py
+++ ip_address_masks.py
@@ -1,54 +1,47 @@
 #! venv/bin/python
-
-complete_address = ["234.67.0.5", "20"] #[i for i in input().split("/")]
+complete_address = [i for i in "234.67.0.5/20".split("/")]
 network_parts = [int(i) for i in complete_address[0].split(".")]
 machine_part = complete_address[1]
 
 
-def get_mask(netmask):
-    bits = []
-    mask = ""
-    for i in range(1, 33):
-        if i <= netmask:
-            bits.append("1")
-        else:
-            bits.append("0")
-        if i % 8 == 0 and i <= netmask:
-            bits.append(".")
-    for i in bits:
-        mask += i
-    return mask
+def get_mask_bits(netmask):
+    return f'{"1" * netmask}{"0" * (32 - netmask)}'
 
 
-def get_binary(octets):
-    bin_ip = ""
-    bin_bdcast = ""
-    for i, v in enumerate(octets):
-        bin_ip += bin(v)[2:].zfill(8)
-        bin_ip += "." if i < len(octets) - 1 else ""
-        if i == 2:
-            bin_bdcast = bin_ip + "1" * 8
-            bin_ip += "0" * 8
-            break
-    return bin_ip, bin_bdcast
+def get_binary_ip(octets, mask_bits):
+    binary = ""
+    for o in octets:
+        binary += format(o, "08b")
+    binary = bin(int(binary, 2) & int(mask_bits, 2))
+    return binary[2:].zfill(32)
 
 
-def get_human_address(bin):
-    hb = ""
-    for i, v in enumerate(bin):
-        hb += str(int(v, 2))
-        hb += "." if i < len(bin) - 1 else ""
-    return hb
+def get_human_address(binary):
+    ip = ""
+    for i in range(0, len(binary) - 7, 8):
+        p = int(binary[i:i+8], 2)
+        ip += f"{p}." 
+    return ip.rstrip(".")
+
+
+def get_human_bdcast(binary, mask):
+    bdcast = ""
+    if mask == "0" * 32:
+        return "255.255.255.255"
+
+    for i in range(0, len(binary) - 14, 8):
+        p = int(binary[i:i + 8], 2) & int(mask[i:i + 8], 2)
+        bdcast += f"{p}."
+    return f"{bdcast}255"
 
 
 if __name__ == "__main__":
-    mask = get_mask(int(machine_part))
-    binary_ip, binary_bdcast = get_binary(network_parts)
+    mask = get_mask_bits(int(machine_part))
+    binary_ip = get_binary_ip(network_parts, mask)
+    human_ip = get_human_address(binary_ip)
 
+    print(human_ip)
     if int(machine_part) <= 24:
-        human_network_address = get_human_address([i for i in binary_ip.split(".")])
-        human_bdcast_address = get_human_address([i for i in binary_bdcast.split(".")])
-        print(human_network_address)
-        print(human_bdcast_address)
+        print(get_human_bdcast(binary_ip, mask))
     else:
-        print("\n".join(complete_address[0]))
+        print(f"{human_ip}")
