5495b0b91d4ff2e789ee8d7d7e7684e5e612fa7d
Jeff <chess.logic@gmail.com>
2021-05-20 16:56:17 -0700

    Add file for new task "target firing"

---
+++ target_firing.py
@@ -0,0 +1,12 @@
+#! venv/binpython3
+
+class Fighter(object):
+    def __init__(self, hp, armor, damage):
+        self.hp = hp
+        self.armor = armor
+        self.damage = damage
+
+
+
+num_ships = int(input())
+
